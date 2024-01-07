71d9d091e113c7eaed158d55d9e6cc885f74a94c
Adam Ruth <adam.ruth01@gmail.com>
2024-01-07 19:01:25 +0100

    Created project

---
+++ src/test/java/com/example/demoadamssqlapi/DemoAdamsSqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoadamssqlapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAdamsSqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
